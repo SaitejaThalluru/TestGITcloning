af95fdb447127c5737d1791cc83ea05c843d2229
Thalluru Saiteja <thalluru.s@mantralabsglobal.com>
2023-06-02 09:31:15 +0530

    New Suite 2

---
+++ Test Suites/TS2.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2232ae16-92ef-4819-a167-51072ace7442</testSuiteGuid>
+</TestSuiteEntity>
